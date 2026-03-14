64c14cf917a1db5bcb8670a7627cb7b29d4c0484
Himanshu Yadav <himanshu12kdma@gmail.com>
2026-03-14 12:56:03 +0530

    add user and video model

---
+++ src/models/video.model.js
@@ -0,0 +1,44 @@
+import mongoose,{Schema} from "mongoose";
+
+//video model
+const videoSchema=new Schema(
+    {
+        videoFile:{
+            type: String,//cloudinary url
+            required: true
+        },
+        thumbnail:{
+            type: String,//cloudinary url
+            required: true
+        },
+        title:{
+            type: String,
+            required: true
+        },
+        description:{
+            type: String,
+            required: true
+        },
+        duration:{
+            type: Number, //cloudinary
+            required: true
+        },
+        views:{
+            type: Number,
+            default:0
+        },
+        isPublished:{
+            type: Boolean,
+            required: true
+        },
+        owner:{
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const Video=mongoose.model("Video",videoSchema)
