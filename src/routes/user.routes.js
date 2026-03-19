27cf19825b7ab1ab472f263c8189b17396185c5e
Himanshu Yadav <himanshu12kdma@gmail.com>
2026-03-20 00:43:53 +0530

    error fixing

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser,loginUser,logoutUser,refreshAccessToken, changeCurrentPassword, getCurrentUser,updateAccountDetails, updateUserAvatar,getUserChannelProfile,getWatchHistory} from "../controllers/user.controller.js";
+import { registerUser,loginUser,logoutUser,refreshAccessToken, changeCurrentPassword, getCurrentUser,updateAccountDetails, updateUserAvatar,getUserChannelProfile,getWatchHistory, updateUserCoverImage} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -26,6 +26,7 @@ router.route("/change-password").post(verifyJWT,changeCurrentPassword)
 router.route("/current-user").get(verifyJWT,getCurrentUser)
 router.route("/update-account").patch(verifyJWT,updateAccountDetails)
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"),updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"),updateUserCoverImage)
 router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
 router.route("/history").get(verifyJWT,getWatchHistory)
 
